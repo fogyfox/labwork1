c7d99384ce22927001a4bfd84ae66705f973303a
Peresvet777 <pavlovicperesvet@gmail.com>
2025-03-14 00:51:23 +0700

    add rectangle

---
+++ pavlovich.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath> // Для функции sqrt
+
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    // Инициализации ширины и высоты
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    // Вычисления периметра
+    double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+
+    // Вычисления площади
+    double calculateArea() {
+        return width * height;
+    }
+
+    // Вычисления длины диагонали
+    double calculateDiagonal() {
+        return sqrt(width * width + height * height);
+    }
+};
+
+int main() {
+    double width, height;
+
+    // Ввод ширины и высоты
+    std::cout << "Введите ширину прямоугольника: ";
+    std::cin >> width;
+    std::cout << "Введите высоту прямоугольника: ";
+    std::cin >> height;
+
+    Rectangle rect(width, height);
+
+    // Вывод результатов
+    std::cout << "Периметр прямоугольника: " << rect.calculatePerimeter() << std::endl;
+    std::cout << "Площадь прямоугольника: " << rect.calculateArea() << std::endl;
+    std::cout << "Длина диагонали прямоугольника: " << rect.calculateDiagonal() << std::endl;
+
+    return 0;
+}
